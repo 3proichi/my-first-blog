46afe30173eb5e218323ce89247959c2652dbe63
3proichi <3proichi@gmail.com>
2023-11-08 13:06:53 +0900

    11/8　更新

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('post/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
     path('post/kadainew/', views.post_new, name='kadai_post_new'),
     path('post/yoteinew/', views.yotei_post_new, name='yotei_post_new'),
     path('post/zyugyounew/', views.zyugyou_post_new, name='zyugyou_post_new'),
     path('post/baitonew/', views.baito_post_new, name='baito_post_new'),    
     path('post/souzikinew/', views.souziki_post_new, name='souziki_post_new'),
     path('post/kazinew/', views.kazi_post_new, name='kazi_post_new'),
+    path('post/ sentakunew/', views.sentaku_post_new, name='sentaku_post_new'),
+
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('post/delete/', views.delete_post, name='post_delete'),
     path('',views.post_start, name='post_start'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,6 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from .forms import ScheduleForm
 from django.shortcuts import redirect
-import datetime
-
-
 
 # Create your views here.
 def post_list(request):
@@ -32,6 +29,46 @@ def post_new(request):
             #authorを追加
             post.author = request.user
             post.published_date = timezone.now()
+            
+            #締め切りまでの日数計算
+            limit = post.deadline - post.published_date
+            #締切の日数分ループ
+            for day in range(limit.days):
+                #探す日
+                searchday = post.published_date+timezone.timedelta(days=day,hours=-post.published_date.hour-9,minutes=-post.published_date.minute,seconds=-post.published_date.second,microseconds=-post.published_date.microsecond)
+                posts = Post.objects.filter(start_data__date=searchday.date())
+
+                zikan = [False] * 24  # 24要素すべてをFalseで初期化
+
+                for post1 in posts:
+                    s = post1.start_data
+                    hour = s.hour
+                    zikan[hour] = True
+                
+                num = 0
+                min = post.spendtime*60;
+                if post.spendtime < 1.0:
+                    for index in range(0,len(zikan)-1):
+                        if not zikan[index]:
+                            post.start_data = searchday + timezone.timedelta(hours=index)
+                            post.finish_data = post.start_data + timezone.timedelta(minutes=min)
+                            break
+                else:
+                    for index in range(0,len(zikan)-1):
+                        if not zikan[index]:
+                            num += 1
+                            if num >= post.spendtime:
+                                if num ==1:
+                                    post.start_data = searchday + timezone.timedelta(hours=(index))
+                                    post.finish_data = post.start_data + timezone.timedelta(minutes=min)
+                                else:
+                                    post.start_data = searchday + timezone.timedelta(hours=(index - (num-1)))
+                                    post.finish_data = post.start_data + timezone.timedelta(minutes=min)
+                                break
+                        else:
+                            num = 0
+
+
             #作成者を追加しつつフォームの変更を保存
             post.save()
             #Post＿detail
@@ -59,6 +96,28 @@ def souziki_post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         default_data = {'title':"掃除機",'text':"の部屋の掃除機をかける"}
+        form2 = PostForm(default_data)
+    return render(request, 'blog/post_edit.html', {'form2': form2})
+
+def sentaku_post_new(request):
+    if request.method == "POST":
+        form2 = PostForm(request.POST)
+        
+        #フォーム内のデータが正常であれば
+        #（すべての必須フィールドが設定され、不正な値が送信されないこと）
+        if form2.is_valid():
+            #フォームを保存
+            #commit=False は Post モデルをまだ保存しないという意味
+            post = form2.save(commit=False)
+            #authorを追加
+            post.author = request.user
+            post.published_date = timezone.now()
+            #作成者を追加しつつフォームの変更を保存
+            post.save()
+            #Post＿detail
+            return redirect('post_detail', pk=post.pk)
+    else:
+        default_data = {'title':"洗濯",'text':"洗濯機回す"}
         form2 = PostForm(default_data)
     return render(request, 'blog/post_edit.html', {'form2': form2})
 
